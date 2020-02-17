c35fc30cc9d35646b823fdb11e7ca2222f3731f7
Panagiotis Roubatsis <proubatsis@gmail.com>
2020-02-16 19:02:01 -0500

    Reverse the sorting of the scrollbar

--- android-app/cashcalculatormodule/src/main/java/org/myoralvillage/cashcalculatormodule/models/CurrencyModel.java
+++ android-app/cashcalculatormodule/src/main/java/org/myoralvillage/cashcalculatormodule/models/CurrencyModel.java
@@ -1,10 +1,9 @@
 package org.myoralvillage.cashcalculatormodule.models;
 
-import android.content.res.Resources;
-
 import org.myoralvillage.cashcalculatormodule.R;
 
 import java.math.BigDecimal;
+import java.util.Comparator;
 import java.util.Currency;
 import java.util.Set;
 import java.util.TreeSet;
@@ -14,7 +13,14 @@ public class CurrencyModel {
     private Set<DenominationModel> denominations;
 
     public CurrencyModel(String currencyCode) {
-        this.denominations = new TreeSet<>();
+        this.denominations = new TreeSet<>(new Comparator<DenominationModel>() {
+            @Override
+            public int compare(DenominationModel o1, DenominationModel o2) {
+                // Sort greatest to least, instead of least to greatest
+                return o2.compareTo(o1);
+            }
+        });
+
         this.currency = Currency.getInstance(currencyCode);
     }
 
