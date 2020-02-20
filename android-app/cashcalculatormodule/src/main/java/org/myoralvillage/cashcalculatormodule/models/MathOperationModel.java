eb74bff26d302b09fdd0b0f4c80dbffaa224dcf6
Panagiotis Roubatsis <proubatsis@gmail.com>
2020-02-20 18:40:42 -0500

    Create AppService and corresponding state models

---
+++ android-app/cashcalculatormodule/src/main/java/org/myoralvillage/cashcalculatormodule/models/MathOperationModel.java
@@ -0,0 +1,56 @@
+package org.myoralvillage.cashcalculatormodule.models;
+
+import androidx.annotation.Nullable;
+
+import java.math.BigDecimal;
+
+public class MathOperationModel {
+    private MathOperationMode mode;
+    private BigDecimal value;
+
+    private MathOperationModel(MathOperationMode mode, BigDecimal value) {
+        this.mode = mode;
+        this.value = value;
+    }
+
+    public MathOperationMode getMode() {
+        return mode;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+    public MathOperationModel copyWithValue(BigDecimal value) {
+        return new MathOperationModel(mode, value);
+    }
+
+    public static MathOperationModel createStandard(BigDecimal value) {
+        return new MathOperationModel(MathOperationMode.STANDARD, value);
+    }
+
+    public static  MathOperationModel createAdd(BigDecimal value) {
+        return new MathOperationModel(MathOperationMode.ADD, value);
+    }
+
+    public static MathOperationModel createSubtract(BigDecimal value) {
+        return new MathOperationModel(MathOperationMode.SUBTRACT, value);
+    }
+
+    public static MathOperationModel createMultiply(BigDecimal value) {
+        return new MathOperationModel(MathOperationMode.MULTIPLY, value);
+    }
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        if (!(obj instanceof MathOperationModel))
+            return false;
+
+        MathOperationModel model = (MathOperationModel) obj;
+        return mode == model.mode && value.equals(model.value);
+    }
+}
---
+++ android-app/cashcalculatormodule/src/test/java/org/myoralvillage/cashcalculatormodule/services/AppServiceTest.java
@@ -0,0 +1,163 @@
+package org.myoralvillage.cashcalculatormodule.services;
+
+import org.junit.Test;
+import org.myoralvillage.cashcalculatormodule.models.AppStateModel;
+import org.myoralvillage.cashcalculatormodule.models.MathOperationModel;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.*;
+
+public class AppServiceTest {
+    @Test
+    public void testDefaultState() {
+        AppService service = new AppService();
+        AppStateModel expected = AppStateModel.getDefault();
+
+        assertEquals(expected, service.getAppState());
+    }
+
+    @Test
+    public void testAddState() {
+        AppService service = new AppService();
+        service.add();
+
+        AppStateModel expected = AppStateModel.getDefault();
+        expected.getOperations().add(MathOperationModel.createAdd(BigDecimal.ZERO));
+
+        assertEquals(expected, service.getAppState());
+    }
+
+    @Test
+    public void testSubtractState() {
+        AppService service = new AppService();
+        service.subtract();
+
+        AppStateModel expected = AppStateModel.getDefault();
+        expected.getOperations().add(MathOperationModel.createSubtract(BigDecimal.ZERO));
+
+        assertEquals(expected, service.getAppState());
+    }
+
+    @Test
+    public void testMultiplyState() {
+        AppService service = new AppService();
+        service.multiply();
+
+        AppStateModel expected = AppStateModel.getDefault();
+        expected.getOperations().add(MathOperationModel.createMultiply(BigDecimal.ZERO));
+
+        assertEquals(expected, service.getAppState());
+    }
+
+    @Test
+    public void testManyStates() {
+        AppService service = new AppService();
+        service.setValue(new BigDecimal(5));
+        service.multiply();
+        service.setValue(new BigDecimal(7));
+        service.add();
+        service.setValue(new BigDecimal(10));
+        service.subtract();
+        service.setValue(new BigDecimal(3));
+        service.setValue(new BigDecimal(2));
+        service.add();
+        service.setValue(new BigDecimal(1));
+
+        AppStateModel expected = AppStateModel.getDefault();
+        expected.getOperations().set(0, MathOperationModel.createStandard(new BigDecimal(5)));
+        expected.getOperations().add(MathOperationModel.createMultiply(new BigDecimal(7)));
+        expected.getOperations().add(MathOperationModel.createAdd(new BigDecimal(10)));
+        expected.getOperations().add(MathOperationModel.createSubtract(new BigDecimal(2)));
+        expected.getOperations().add(MathOperationModel.createAdd(new BigDecimal(1)));
+
+        assertEquals(expected, service.getAppState());
+    }
+
+    @Test
+    public void testCalculation() {
+        AppService service = new AppService();
+        service.setValue(new BigDecimal(5));
+        service.multiply();
+        service.setValue(new BigDecimal(7));
+        service.add();
+        service.setValue(new BigDecimal(10));
+        service.subtract();
+        service.setValue(new BigDecimal(3));
+        service.setValue(new BigDecimal(2));
+        service.add();
+        service.setValue(new BigDecimal(1));
+        service.calculate();
+
+        BigDecimal expected = new BigDecimal(42);
+        assertEquals(expected, service.getValue());
+    }
+
+    @Test
+    public void testCalculateState() {
+        AppService service = new AppService();
+        service.setValue(new BigDecimal(5));
+        service.multiply();
+        service.setValue(new BigDecimal(7));
+        service.add();
+        service.setValue(new BigDecimal(10));
+        service.subtract();
+        service.setValue(new BigDecimal(3));
+        service.setValue(new BigDecimal(2));
+        service.add();
+        service.setValue(new BigDecimal(1));
+        service.calculate();
+
+        AppStateModel expected = AppStateModel.getDefault();
+        expected.getOperations().set(0, MathOperationModel.createStandard(new BigDecimal(5)));
+        expected.getOperations().add(MathOperationModel.createMultiply(new BigDecimal(7)));
+        expected.getOperations().add(MathOperationModel.createAdd(new BigDecimal(10)));
+        expected.getOperations().add(MathOperationModel.createSubtract(new BigDecimal(2)));
+        expected.getOperations().add(MathOperationModel.createAdd(new BigDecimal(1)));
+        expected.getOperations().add(MathOperationModel.createStandard(new BigDecimal(42)));
+
+        assertEquals(expected, service.getAppState());
+    }
+
+    @Test
+    public void testAdditionalCalculation() {
+        AppService service = new AppService();
+        service.setValue(new BigDecimal(5));
+        service.multiply();
+        service.setValue(new BigDecimal(7));
+        service.add();
+        service.setValue(new BigDecimal(10));
+        service.subtract();
+        service.setValue(new BigDecimal(3));
+        service.setValue(new BigDecimal(2));
+        service.add();
+        service.setValue(new BigDecimal(1));
+        service.calculate();
+        service.setValue(new BigDecimal(50));
+        service.add();
+        service.setValue(new BigDecimal(9));
+        service.calculate();
+
+        BigDecimal expected = new BigDecimal(59);
+        assertEquals(expected, service.getValue());
+    }
+
+    @Test
+    public void testOrderOfOperations() {
+        AppService service = new AppService();
+        service.setValue(new BigDecimal(5));
+        service.add();
+        service.setValue(new BigDecimal(7));
+        service.add();
+        service.setValue(new BigDecimal(10));
+        service.multiply();
+        service.setValue(new BigDecimal(3));
+        service.setValue(new BigDecimal(2));
+        service.subtract();
+        service.setValue(new BigDecimal(1));
+        service.calculate();
+
+        BigDecimal expected = new BigDecimal(31);
+        assertEquals(expected, service.getValue());
+    }
+}
