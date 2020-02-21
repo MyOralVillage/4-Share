e9c415e38b67f7846515a0bb002eb098b0920b6b
Panagiotis Roubatsis <proubatsis@gmail.com>
2020-02-20 19:27:08 -0500

    Make activity fullscreen

--- android-app/app/src/main/java/org/myoralvillage/cashcalculator/MainActivity.java
+++ android-app/app/src/main/java/org/myoralvillage/cashcalculator/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -29,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         service = new AppService();
 
