3e5cb414383cf0df125b3b3aa5b58d9fe2e8ee38
Panagiotis Roubatsis <proubatsis@gmail.com>
2020-02-26 14:48:04 -0500

    Refactor MainActivity for readability

--- android-app/app/src/main/java/org/myoralvillage/cashcalculator/MainActivity.java
+++ android-app/app/src/main/java/org/myoralvillage/cashcalculator/MainActivity.java
@@ -58,10 +58,108 @@ public class MainActivity extends AppCompatActivity {
         else service = new AppService();
 
         sumView = findViewById(R.id.sum_view);
-        final CurrencyScrollbarView currencyScrollbarView = findViewById(R.id.currency_scrollbar);
-        currencyScrollbarView.setCurrency("PKR");
-        this.currCurrency = currencyScrollbarView.getCurrency();
+        initializeCurrencyScrollbar();
+        initializeCountingView();
+        initializeCalculateButton();
+        initializeClearButton();
+        initializeHistoryButtons();
+        initializeNumberpad();
 
+        updateAll();
+    }
+
+    private void initializeHistoryButtons() {
+        enterHistoryButton = findViewById(R.id.enter_history_button);
+        rightHistoryButton = findViewById(R.id.right_history_button);
+        leftHistoryButton = findViewById(R.id.left_history_button);
+
+        enterHistoryButton.setOnClickListener((e) -> {
+            service.enterHistorySlideshow();
+            updateAll();
+        });
+
+        rightHistoryButton.setOnClickListener((e) -> {
+            service.gotoNextHistorySlide();
+            updateAll();
+        });
+
+        leftHistoryButton.setOnClickListener((e) -> {
+            service.gotoPreviousHistorySlide();
+            updateAll();
+        });
+
+        updateAll();
+    }
+
+    private void updateHistoryButtons() {
+        if (service.isInHistorySlideshow()) {
+            enterHistoryButton.setVisibility(View.INVISIBLE);
+            leftHistoryButton.setVisibility(View.VISIBLE);
+            rightHistoryButton.setVisibility(View.VISIBLE);
+        } else {
+            if (service.getAppState().getOperations().size() == 1)
+                enterHistoryButton.setVisibility(View.INVISIBLE);
+            else enterHistoryButton.setVisibility(View.VISIBLE);
+
+            rightHistoryButton.setVisibility(View.INVISIBLE);
+            leftHistoryButton.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void initializeCalculateButton() {
+        calculateButton = findViewById(R.id.calculate_button);
+        calculateButton.setOnClickListener((e) -> {
+            if (!service.isInHistorySlideshow()) {
+                service.calculate();
+                updateAll();
+            }
+        });
+    }
+
+    private void updateCalculateButton() {
+        calculateButton.setVisibility(View.VISIBLE);
+        switch(service.getOperationMode()) {
+            case STANDARD:
+                calculateButton.setVisibility(View.INVISIBLE);
+                break;
+            case ADD:
+                calculateButton.setImageResource(R.drawable.operator_plus);
+                break;
+            case SUBTRACT:
+                calculateButton.setImageResource(R.drawable.operator_minus);
+                break;
+            case MULTIPLY:
+                calculateButton.setImageResource(R.drawable.operator_times);
+                break;
+        }
+    }
+
+    private void initializeClearButton() {
+        clearButton = findViewById(R.id.clear_button);
+        clearButton.setOnClickListener((e) -> {
+            service.reset();
+            updateAll();
+        });
+    }
+
+    private void updateClearButton() {
+        if (service.getOperationMode() == MathOperationModel.MathOperationMode.STANDARD && service.getValue().equals(BigDecimal.ZERO))
+            clearButton.setVisibility(View.INVISIBLE);
+        else
+            clearButton.setVisibility(View.VISIBLE);
+    }
+
+    private void updateSumView() {
+        if (service.getValue().compareTo(BigDecimal.ZERO) < 0)
+            sumView.setTextColor(getResources().getColor(R.color.negativeSum));
+        else
+            sumView.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+
+        sumView.setText(String.format(Locale.CANADA, "%s %s",
+                currCurrency.getCurrency().getSymbol(), service.getValue()));
+    }
+
+    private void initializeCountingView() {
         Display display = getWindowManager().getDefaultDisplay();
         Point size = new Point();
         display.getSize(size);
@@ -70,25 +168,6 @@ public class MainActivity extends AppCompatActivity {
 
         countingTableView = findViewById(R.id.counting_table);
         countingTableView.initDenominationModels(currCurrency.getDenominations(), width, height);
-
-        currencyScrollbarView.setCurrencyTapListener(denomination -> {
-            service.setValue(service.getValue().add(denomination.getValue()));
-            refreshCountingTable();
-        });
-
-        calculateButton = findViewById(R.id.calculate_button);
-        calculateButton.setOnClickListener((e) -> {
-            if (!service.isInHistorySlideshow()) {
-                service.calculate();
-                refreshCountingTable();
-            }
-        });
-
-        clearButton = findViewById(R.id.clear_button);
-        clearButton.setOnClickListener((e) -> {
-            service.reset();
-            refreshCountingTable();
-        });
 
         countingTableView.setOnTouchListener(new SwipeListener(MainActivity.this) {
             @Override
@@ -135,9 +214,25 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
 
-        refreshCountingTable();
+    private void updateCountingTable() {
+        countingTableView.setDenominations(currCurrency.getDenominations().iterator(),
+                countingService.allocate(service.getValue(), currCurrency), service.getValue());
+    }
 
+    private void initializeCurrencyScrollbar() {
+        final CurrencyScrollbarView currencyScrollbarView = findViewById(R.id.currency_scrollbar);
+        currencyScrollbarView.setCurrency("PKR");
+        this.currCurrency = currencyScrollbarView.getCurrency();
+
+        currencyScrollbarView.setCurrencyTapListener(denomination -> {
+            service.setValue(service.getValue().add(denomination.getValue()));
+            updateAll();
+        });
+    }
+
+    private void initializeNumberpad() {
         numberPadView = findViewById(R.id.number_pad);
         entryView = findViewById(R.id.entry);
         entryView.setVisibility(View.INVISIBLE);
@@ -153,7 +248,7 @@ public class MainActivity extends AppCompatActivity {
                     stringBuilder.setLength(0);
                     entryView.setVisibility(View.INVISIBLE);
                     numberPadView.setVisibility(View.INVISIBLE);
-                    refreshCountingTable();
+                    updateAll();
                     return;
                 case 2:
                     if (stringBuilder.length() > 0) {
@@ -168,84 +263,14 @@ public class MainActivity extends AppCompatActivity {
                     currCurrency.getCurrency().getSymbol(),
                     stringBuilder.length() > 0 ? stringBuilder.toString() : "0"));
         });
-
-        initializeHistoryButtons();
     }
 
-    private void initializeHistoryButtons() {
-        enterHistoryButton = findViewById(R.id.enter_history_button);
-        rightHistoryButton = findViewById(R.id.right_history_button);
-        leftHistoryButton = findViewById(R.id.left_history_button);
-
-        enterHistoryButton.setOnClickListener((e) -> {
-            service.enterHistorySlideshow();
-            refreshCountingTable();
-        });
-
-        rightHistoryButton.setOnClickListener((e) -> {
-            service.gotoNextHistorySlide();
-            refreshCountingTable();
-        });
-
-        leftHistoryButton.setOnClickListener((e) -> {
-            service.gotoPreviousHistorySlide();
-            refreshCountingTable();
-        });
-
-        refreshHistoryButtons();
-    }
-
-    private void refreshHistoryButtons() {
-        if (enterHistoryButton != null && rightHistoryButton != null && leftHistoryButton != null) {
-            if (service.isInHistorySlideshow()) {
-                enterHistoryButton.setVisibility(View.INVISIBLE);
-                leftHistoryButton.setVisibility(View.VISIBLE);
-                rightHistoryButton.setVisibility(View.VISIBLE);
-            } else {
-                if (service.getAppState().getOperations().size() == 1)
-                    enterHistoryButton.setVisibility(View.INVISIBLE);
-                else enterHistoryButton.setVisibility(View.VISIBLE);
-
-                rightHistoryButton.setVisibility(View.INVISIBLE);
-                leftHistoryButton.setVisibility(View.INVISIBLE);
-            }
-        }
-    }
-
-    private void refreshCountingTable() {
-        calculateButton.setVisibility(View.VISIBLE);
-        switch(service.getOperationMode()) {
-            case STANDARD:
-                calculateButton.setVisibility(View.INVISIBLE);
-                break;
-            case ADD:
-                calculateButton.setImageResource(R.drawable.operator_plus);
-                break;
-            case SUBTRACT:
-                calculateButton.setImageResource(R.drawable.operator_minus);
-                break;
-            case MULTIPLY:
-                calculateButton.setImageResource(R.drawable.operator_times);
-                break;
-        }
-
-        refreshHistoryButtons();
-
-        if (service.getOperationMode() == MathOperationModel.MathOperationMode.STANDARD && service.getValue().equals(BigDecimal.ZERO))
-            clearButton.setVisibility(View.INVISIBLE);
-        else
-            clearButton.setVisibility(View.VISIBLE);
-
-        if (service.getValue().compareTo(BigDecimal.ZERO) < 0)
-            sumView.setTextColor(getResources().getColor(R.color.negativeSum));
-        else
-            sumView.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
-
-        sumView.setText(String.format(Locale.CANADA, "%s %s",
-                currCurrency.getCurrency().getSymbol(), service.getValue()));
-
-        countingTableView.setDenominations(currCurrency.getDenominations().iterator(),
-                countingService.allocate(service.getValue(), currCurrency), service.getValue());
+    private void updateAll() {
+        updateCalculateButton();
+        updateHistoryButtons();
+        updateClearButton();
+        updateSumView();
+        updateCountingTable();
     }
 
     private void switchState() {
