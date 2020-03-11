297ba84a52142aa44988d12849beb9957083afb0
Panagiotis Roubatsis <proubatsis@gmail.com>
2020-03-11 13:11:15 -0400

    Remove unused imports

--- android-app/app/src/main/java/org/myoralvillage/cashcalculator/fragments/CashCalculatorFragment.java
+++ android-app/app/src/main/java/org/myoralvillage/cashcalculator/fragments/CashCalculatorFragment.java
@@ -1,13 +1,10 @@
 package org.myoralvillage.cashcalculator.fragments;
 
 import android.content.Intent;
-import android.graphics.Point;
 import android.os.Bundle;
-import android.view.Display;
 import android.view.View;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.fragment.app.Fragment;
 
