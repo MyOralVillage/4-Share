9871450fb0cec4b59d5dce67e9d1216eb010514b
ajyang2000 <alexander_yang_12@hotmail.com>
2020-03-12 22:55:23 -0400

    Added javadoc for services.

--- android-app/cashcalculatormodule/src/main/java/org/myoralvillage/cashcalculatormodule/services/AppService.java
+++ android-app/cashcalculatormodule/src/main/java/org/myoralvillage/cashcalculatormodule/services/AppService.java
@@ -8,30 +8,69 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A service class used to perform the main operations of the <code>CashCalculator</code>.
+ * @author Hamza Mahfooz
+ * @author Peter Panagiotis Roubatsis
+ * @see java.lang.Object
+ */
 public class AppService {
+    /**
+     * A constant variable used whenever the value displayed is zero.
+     */
     private final static BigDecimal INITIAL_VALUE = new BigDecimal(0);
+
+    /**
+     * This variable is used to store each state of the <code>Cash Calculator</code>.
+     *
+     * @see AppStateModel
+     */
     private AppStateModel appState;
 
+    /**
+     * Constructs a new <code>AppService</code> where the appState is in the default mode.
+     *
+     * @see AppStateModel
+     */
     public AppService() {
         appState = AppStateModel.getDefault();
         resetCurrentOperation();
     }
 
+    /**
+     * Constructs a new <code>AppService</code> with the specified appState.
+     *
+     * @param appState the appState of this service.
+     * @see AppStateModel
+     */
     public AppService(AppStateModel appState) {
         this.appState = appState;
         resetCurrentOperation();
     }
 
+    /**
+     * Returns the appState associated with this service.
+     *
+     * @return the appState of this service.
+     */
     public AppStateModel getAppState() {
         return appState;
     }
 
+    /**
+     * Changes the appState between Image mode and Numeric mode.
+     *
+     * @see AppStateModel
+     */
     public void switchAppMode() {
         if (appState.getAppMode() == AppStateModel.AppMode.IMAGE)
             appState.setAppMode(AppStateModel.AppMode.NUMERIC);
         else appState.setAppMode(AppStateModel.AppMode.IMAGE);
     }
 
+    /**
+     * Undo the previous change performed on the <code>CashCalculator</code>.
+     */
     public void reset() {
         AppStateModel.AppMode mode = appState.getAppMode();
         appState = AppStateModel.getDefault();
@@ -39,31 +78,62 @@ public class AppService {
         resetCurrentOperation();
     }
 
+    /**
+     * Undo the the previous operation performed on the <code>CashCalculator</code>.
+     */
     private void resetCurrentOperation() {
         appState.setCurrentOperationIndex(appState.getOperations().size() - 1);
     }
 
+    /**
+     * Returns the mathematical operation of the current state of the <code>CashCalculator</code>.
+     *
+     * @return the mathematical operation of the <code>CashCalculator</code>.
+     */
     public MathOperationMode getOperationMode() {
         return appState.getCurrentOperation().getMode();
     }
 
+    /**
+     * The current total value displayed on the <code>CashCalculator</code>.
+     *
+     * @return The total value of the <code>CashCalculator</code>.
+     */
     public BigDecimal getValue() {
         return appState.getCurrentOperation().getValue();
     }
 
+    /**
+     * Changes the total value displayed on the <code>CashCalculator</code>.
+     *
+     * @param value the new total value of the <code>CashCalculator</code>.
+     */
     public void setValue(BigDecimal value) {
         MathOperationModel currentOperation = appState.getCurrentOperation();
         currentOperation.setValue(value);
     }
 
+    /**
+     * Checks if the <code>CashCalculator</code> is in the history mode.
+     *
+     * @return True if the application is in the history mode; False otherwise
+     */
     public boolean isInHistorySlideshow() {
         return appState.isInHistorySlideshow();
     }
 
+    /**
+     * Enter the history mode.
+     */
     public void enterHistorySlideshow() {
         appState.setCurrentOperationIndex(0);
     }
 
+    /**
+     * Show the next operation performed on the <code>CashCalculator</code>. If this current history
+     * slide was the last element in the history slides, return to the standard mode, removing the
+     * most recent state that occurred before entering the history mode.
+     */
     public void gotoNextHistorySlide() {
         if (isInHistorySlideshow())
             appState.setCurrentOperationIndex(appState.getCurrentOperationIndex() + 1);
@@ -72,38 +142,73 @@ public class AppService {
             changeCalculation();
     }
 
+    /**
+     * Removes the most recent state that occurred and change state to standard mode.
+     */
     private void changeCalculation() {
         appState.getOperations().remove(appState.getOperations().size() - 1);
         calculate();
     }
 
+    /**
+     * Show the previous operation performed on the <code>CashCalculator</code>. Returns nothing if
+     * the current history slide before executing this function is the first operation performed on
+     * the application.
+     */
     public void gotoPreviousHistorySlide() {
         if (isInHistorySlideshow() && appState.getCurrentOperationIndex() > 0)
             appState.setCurrentOperationIndex(appState.getCurrentOperationIndex() - 1);
     }
 
+    /**
+     * Adds a new ADD state to the list of mathematical operations in the variable, <code>appState</code>
+     */
     public void add() {
         addOperation(MathOperationModel.createAdd(INITIAL_VALUE));
     }
 
+    /**
+     * Adds a new SUBTRACT state to the list of mathematical operations in the variable, <code>appState</code>
+     */
     public void subtract() {
         addOperation(MathOperationModel.createSubtract(INITIAL_VALUE));
     }
 
+    /**
+     * Adds a new MULTIPLY state to the list of mathematical operations in the variable, <code>appState</code>
+     */
     public void multiply() {
         addOperation(MathOperationModel.createMultiply(INITIAL_VALUE));
     }
 
+    /**
+     * Adds a new STANDARD state to the list of mathematical operations in the variable, <code>appState</code>
+     */
     public void calculate() {
         BigDecimal result = calculateOperationsResult(appState.getOperations());
         addOperation(MathOperationModel.createStandard(result));
     }
 
+    /**
+     * Helper function used to insert a mathematical operation to the list of operations in the
+     * variable, <code>appState</code>. Used in the functions, <code>add()</code>, <code>subtract()</code>,
+     * <code>multiply()</code> and <code>calculate</code>.
+     * @param operation The mathematical operation to be added
+     *
+     * @see MathOperationModel
+     */
     private void addOperation(MathOperationModel operation) {
         this.appState.getOperations().add(operation);
         resetCurrentOperation();
     }
 
+    /**
+     * Performs the calculations of the operation that has occurred.
+     *
+     * @param operations the list of operations performed.
+     * @return the result of the operations.
+     * @see MathOperationModel
+     */
     private static BigDecimal calculateOperationsResult(List<MathOperationModel> operations) {
         if (operations.size() == 0)
             return BigDecimal.ZERO;
@@ -130,6 +235,15 @@ public class AppService {
         return BigDecimal.ZERO;
     }
 
+    /**
+     * Returns the index of the most recent occurrence of the specified mode in the list of operations.
+     *
+     * @param operations the list of operations to be searched.
+     * @param mode the mode that is being searched for.
+     * @return the index of the most recent occurrence of the mode.
+     *
+     * @see MathOperationModel
+     */
     private static int findLastOperationOfMode(List<MathOperationModel> operations, MathOperationMode mode) {
         int index = -1;
 
@@ -140,6 +254,14 @@ public class AppService {
         return index;
     }
 
+    /**
+     * Helper function used in <code>calculateOperationResults</code> that performs the calculation
+     * and updates the list of operations.
+     *
+     * @param operations the list of mathematical operations.
+     * @param index the index of the current operation to be performed.
+     * @return the updated list of mathematical operations.
+     */
     private static List<MathOperationModel> collapseOperationAtIndex(List<MathOperationModel> operations, int index) {
         int prevIndex = index - 1;
         MathOperationModel current = operations.get(index);
