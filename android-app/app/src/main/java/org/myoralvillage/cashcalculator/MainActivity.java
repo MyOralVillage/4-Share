8068ea5556905b7755e70dd2a775127e6c28583c
jiahengli <jiahengwl.li@mail.utoronto.ca>
2020-03-15 09:43:32 -0400

    lijiahe5 update mirrors errors

--- android-app/app/src/main/java/org/myoralvillage/cashcalculator/MainActivity.java
+++ android-app/app/src/main/java/org/myoralvillage/cashcalculator/MainActivity.java
@@ -1,22 +1,11 @@
 package org.myoralvillage.cashcalculator;
 
-import java.util.Currency;
-
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
-import org.myoralvillage.cashcalculator.fragments.CashCalculatorFragment;
-import org.myoralvillage.cashcalculatormodule.models.CurrencyModel;
-import org.myoralvillage.cashcalculatormodule.views.CurrencyScrollbarView;
-
-import java.math.BigDecimal;
-
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Context;
-
-//import static java.security.AccessController.getContext;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -28,16 +17,5 @@ public class MainActivity extends AppCompatActivity{
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-    }
-
-    public  Currency getCurrency(){
-        //CurrencyModel currency = CurrencyModel.loadCurrencyModel(SettingActivity.getSettingService().getCurrencyName(), getResources(), getContext());
-        CurrencyModel currency = new CurrencyModel(SettingActivity.getSettingService().getCurrencyName());
-        return currency.getCurrency();
-    }
-
-    public BigDecimal getCurrentValue(){
-        CashCalculatorFragment fragment = new CashCalculatorFragment();
-        return fragment.getValue();
     }
 }
