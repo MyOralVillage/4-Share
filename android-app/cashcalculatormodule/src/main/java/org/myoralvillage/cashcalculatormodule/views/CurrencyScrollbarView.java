9418f4f8d69173f7cd660fc04b92072fb8682437
ajyang2000 <alexander_yang_12@hotmail.com>
2020-03-28 10:08:52 -0400

    Remove unused imports

--- android-app/cashcalculatormodule/src/main/java/org/myoralvillage/cashcalculatormodule/views/CurrencyScrollbarView.java
+++ android-app/cashcalculatormodule/src/main/java/org/myoralvillage/cashcalculatormodule/views/CurrencyScrollbarView.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.os.Handler;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.MotionEvent;
